42fb982bc247786ca581f08687b9819b8eed9888
Rayzr522 <rayzr522@gmail.com>
2016-09-28 16:09:56 -0400

    Fixed the glaring mistakes in I Al Istannen's code ;)
    Just kidding…

--- src/main/java/com/perceivedev/randomlore/listener/LoreListener.java
+++ src/main/java/com/perceivedev/randomlore/listener/LoreListener.java
@@ -1,27 +1,20 @@
 package com.perceivedev.randomlore.listener;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import com.perceivedev.randomlore.RandomLore;
 import com.perceivedev.randomlore.data.LoreObject;
-import com.perceivedev.randomlore.random.RandomPicker;
 
 /**
  * The listener
@@ -33,6 +26,12 @@ public class LoreListener implements Listener {
     @EventHandler
     public void updateOpenInventory(InventoryOpenEvent e) {
         replaceInInventory(e.getPlayer().getInventory());
+        replaceInInventory(e.getInventory());
+    }
+    
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        replaceInInventory(e.getWhoClicked().getInventory());
         replaceInInventory(e.getInventory());
     }
 
@@ -83,39 +82,5 @@ public class LoreListener implements Listener {
     private boolean isIdentifier(String string) {
         return DETECTION_PATTERN.matcher(string).matches();
     }
-
-    private DecimalFormat format = new DecimalFormat("#.##");
-
-    // // TODO: 28.09.2016 A debug event. Remove it. 
-    // A debug event. Notifies you about the weights for the lores, freshly calculated 
-    @EventHandler
-    public void onInteract(PlayerInteractEvent e) {
-        if (e.getAction() != Action.LEFT_CLICK_AIR) {
-            return;
-        }
-        
-        ItemStack itemStack = new ItemStack(Material.STICK);
-        ItemMeta itemMeta = itemStack.getItemMeta();
-        itemMeta.setLore(Arrays.asList("Sth", "%LORE%"));
-        itemStack.setItemMeta(itemMeta);
-        e.getPlayer().getInventory().addItem(itemStack);
-        
-        RandomPicker<LoreObject> randomPicker = RandomLore.getInstance().getRandomPicker();
-
-        Map<LoreObject, Integer> loreMap = new HashMap<>();
-        for (int i = 0; i < 1000000; i++) {
-            LoreObject random = randomPicker.getRandom();
-            int amount = loreMap.getOrDefault(random, 0) + 1;
-            loreMap.put(random, amount);
-        }
-
-        int sum = loreMap.values().stream().mapToInt(Integer::intValue).sum();
-
-        for (Entry<LoreObject, Integer> entry : loreMap.entrySet()) {
-            double value = entry.getValue() / (double) sum;
-            String formatted = format.format(value) + " '" + entry.getKey().getLore() + "'";
-            e.getPlayer().sendMessage(formatted);
-        }
-        e.getPlayer().sendMessage(" ");
-    }
+    
 }
